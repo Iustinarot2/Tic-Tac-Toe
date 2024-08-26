60efdd763051f41513026e99e343d70f5b65bc7b
Iustinarot2 <127149995+Iustinarot2@users.noreply.github.com>
2024-08-26 18:34:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -174,16 +174,3 @@ def flip_player():
   
 
 play_game()
-
-
-
-# board
-# display board
-# play game
-# handle turn
-# check win
-  # check rows 
-  # check columns
-  # check diagonals 
-# check tie 
-# flip player
